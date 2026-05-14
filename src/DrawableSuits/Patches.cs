8a77a677680c1855859bd00541cf9036ac02876a
Toxc <125293391+ToxcGang@users.noreply.github.com>
2026-05-13 21:55:12 -0600

    Add Harmony patches for drawable suits functionality
    This file contains Harmony patches for various methods in the StartOfRound, UnlockableSuit, and PlayerControllerB classes to apply drawable suits functionality.

---
+++ src/DrawableSuits/Patches.cs
@@ -0,0 +1,74 @@
+using GameNetcodeStuff;
+using HarmonyLib;
+
+namespace DrawableSuits;
+
+[HarmonyPatch(typeof(StartOfRound))]
+internal static class StartOfRoundPatches
+{
+    [HarmonyPostfix]
+    [HarmonyPatch("LoadUnlockables")]
+    private static void LoadUnlockablesPostfix()
+    {
+        DrawableSuitsPlugin.Registry?.ReapplyAll();
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch("PositionSuitsOnRack")]
+    private static void PositionSuitsOnRackPostfix()
+    {
+        DrawableSuitsPlugin.Registry?.ReapplyAll();
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(nameof(StartOfRound.SyncSuitsClientRpc))]
+    private static void SyncSuitsClientRpcPostfix()
+    {
+        DrawableSuitsPlugin.Registry?.ReapplyAll();
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch("OnPlayerConnectedClientRpc")]
+    private static void OnPlayerConnectedClientRpcPostfix()
+    {
+        DrawableSuitsPlugin.Registry?.ReapplyAll();
+        DrawableSuitsPlugin.Sync?.RequestActiveDesigns();
+    }
+}
+
+[HarmonyPatch(typeof(UnlockableSuit))]
+internal static class UnlockableSuitPatches
+{
+    [HarmonyPostfix]
+    [HarmonyPatch(nameof(UnlockableSuit.SwitchSuitForPlayer))]
+    private static void SwitchSuitForPlayerPostfix(PlayerControllerB player)
+    {
+        DrawableSuitsPlugin.Registry?.ApplyToPlayer(player);
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(nameof(UnlockableSuit.SwitchSuitForAllPlayers))]
+    private static void SwitchSuitForAllPlayersPostfix()
+    {
+        DrawableSuitsPlugin.Registry?.ReapplyAll();
+    }
+}
+
+[HarmonyPatch(typeof(PlayerControllerB))]
+internal static class PlayerControllerBPatches
+{
+    [HarmonyPostfix]
+    [HarmonyPatch(nameof(PlayerControllerB.ConnectClientToPlayerObject))]
+    private static void ConnectClientToPlayerObjectPostfix(PlayerControllerB __instance)
+    {
+        DrawableSuitsPlugin.Registry?.ApplyToPlayer(__instance);
+        DrawableSuitsPlugin.Sync?.RequestActiveDesigns();
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(nameof(PlayerControllerB.SpawnPlayerAnimation))]
+    private static void SpawnPlayerAnimationPostfix(PlayerControllerB __instance)
+    {
+        DrawableSuitsPlugin.Registry?.ApplyToPlayer(__instance);
+    }
+}
