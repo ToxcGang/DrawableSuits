b5e846add2f98c1cb4486a03471c05611f026259
Toxc <125293391+ToxcGang@users.noreply.github.com>
2026-05-13 21:56:33 -0600

    Add PluginInfo class for DrawableSuits plugin

---
+++ src/DrawableSuits/PluginInfo.cs
@@ -0,0 +1,8 @@
+namespace DrawableSuits;
+
+internal static class PluginInfo
+{
+    public const string Guid = "com.toxcgang.drawablesuits";
+    public const string Name = "DrawableSuits";
+    public const string Version = "0.1.0";
+}
