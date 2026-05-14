d49c09ae314ed7aa77477bd8de3e6d3db569a71b
Toxc <125293391+ToxcGang@users.noreply.github.com>
2026-05-13 22:08:41 -0600

    Add SuitTextureState class for suit texture management

---
+++ src/DrawableSuits/SuitTextureState.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace DrawableSuits;
+
+internal sealed class SuitTextureState
+{
+    public int SuitId;
+    public string SuitName;
+    public Material OriginalMaterial;
+    public Texture OriginalTexture;
+    public Material RuntimeMaterial;
+    public Texture2D BaseTexture;
+    public Texture2D EditableTexture;
+    public string ActiveDesignName;
+}
